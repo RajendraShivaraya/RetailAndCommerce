858f93fb4462d3973aaa8c03fcdad430dc6f90dd
Rajendra Shivaraya <Rajendra.Shivaraya@hotmail.com>
2024-03-20 00:53:29 +0000

    Commerce proxy feature and common project for all controller classes

---
+++ RajendraPOSExtensions/Rajendra.Extensions/CommerceRuntime/RajendraRetailServerExtensions/ODataControllers/RajGetStoreHoursController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Dynamics.Commerce.Runtime.Hosting.Contracts;
+using System.Xml;
+using Runtime.Extension.StoreHours.RequestsAndResponses;
+using Runtime.Extension.StoreHours.Entities;
+using Microsoft.Dynamics.Commerce.Runtime.DataModel;
+using Microsoft.Dynamics.Commerce.Runtime;
+using RajStoreHours = Runtime.Extension.StoreHours;
+using System.Threading.Tasks;
+
+namespace RajendraRetailServerExtensions.ODataControllers
+{
+    [RoutePrefix("RajendraEntityAPI")]
+    [BindEntity(typeof(RajStoreHours.Entities.RajStoreDayHoursEntity))]
+    public class RajGetStoreHoursController : IController
+    {
+        /// <summary>
+        /// Gets the controller name.
+        /// </summary>
+        public string ControllerName
+        {
+            get { return "Rajendra Get Store Hours"; }
+        }
+
+        [HttpPost]
+        [Authorization(CommerceRoles.Anonymous, CommerceRoles.Customer, CommerceRoles.Device, CommerceRoles.Employee, CommerceRoles.Application)]
+        public async Task<PagedResult<RajStoreDayHoursEntity>> GetStoreHoursByRecId(IEndpointContext context, string storeNumber, QueryResultSettings queryResultSettings)
+        {
+            RajGetStoreHoursDataRequest request = new RajGetStoreHoursDataRequest(storeNumber);
+            RajGetStoreHoursDataResponse response = await context.ExecuteAsync<RajGetStoreHoursDataResponse>(request).ConfigureAwait(false);
+            return response.DayHours;
+        }
+
+        [HttpGet]
+        [Authorization(CommerceRoles.Anonymous, CommerceRoles.Customer, CommerceRoles.Device, CommerceRoles.Employee, CommerceRoles.Application)]
+        public Task<string> GetMyName()
+        {
+            return Task.FromResult("Rajendra Sample Get APIs");
+        }
+
+        [HttpGet]
+        [Authorization(CommerceRoles.Anonymous, CommerceRoles.Customer, CommerceRoles.Device, CommerceRoles.Employee, CommerceRoles.Application)]
+        public Task<bool> GetSampleForBoolean()
+        {
+            return Task.FromResult(true);
+        }
+    }
+}
---
+++ RajendraPOSExtensions/Rajendra.Extensions/CommerceRuntime/RajendraRetailServerExtensions/ODataControllers/RajSampleAPIController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Dynamics.Commerce.Runtime.DataModel;
+using Microsoft.Dynamics.Commerce.Runtime.Hosting.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RajendraRetailServerExtensions.ODataControllers
+{
+    public class RajSampleAPIController : IController
+    {
+        /// <summary>
+        /// A simple GET endpoint to demonstrate GET endpoints on an unbound controller.
+        /// </summary>
+        /// <returns>A simple true value to indicate the endpoint was reached.</returns>
+        [HttpGet]
+        [Authorization(CommerceRoles.Anonymous, CommerceRoles.Application, CommerceRoles.Customer, CommerceRoles.Device, CommerceRoles.Employee, CommerceRoles.Storefront)]
+        public Task<bool> RajSampleGet()
+        {
+            return Task.FromResult(true);
+        }
+
+        /// <summary>
+        /// A simple POST endpoint to demonstrate POST endpoints on an unbound controller.
+        /// </summary>
+        /// <returns>A simple true value to indicate the endpoint was reached.</returns>
+        [HttpPost]
+        [Authorization(CommerceRoles.Customer, CommerceRoles.Device, CommerceRoles.Employee)]
+        public Task<bool> RajSamplePost()
+        {
+            return Task.FromResult(true);
+        }
+    }
+}
